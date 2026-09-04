e111e2b742f28742ed92a0a915880c82a267c08c
Julian Castro <juliancastroh89@gmail.com>
2026-09-03 23:20:00 -0500

    Organizacion estructura del proyecto

---
+++ src/test/java/com/Ayr/AyrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Ayr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AyrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
